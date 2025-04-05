9512bacbfaa0a17cf0538277af5f9cdb02e17237
Deepanshu negi <deepanshunegi98@gmail.com>
2025-04-05 10:46:36 +0530

    Fixed case-sensitive import for Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import UserLogin from "./pages/UserLogin";
 import UserSignup from "./pages/UserSignup";
 import CaptainSignup from "./pages/CaptainSignup";
